ebbb4f8022a2e1ebbb6c045eb3b1337498eb15b7
Kalpesh wagh <186034915+kalpesh-73@users.noreply.github.com>
2026-03-29 12:41:15 +0530

    utils file

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler=(requestHandler)=>{
+   (req,res,next)=>{
+Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+   }
+};
+
+
+
+
+export {asyncHandler} ;
+/*
+const aasynchandler=(fn)=>{};
+const asyncHandler=(fn)=>{async()=>{}};
+const asyncHandler=(fn)=>async()=>{};
+
+const asyncHandler=(fn)=>async(res,req,next)=>{
+try{
+    await fn(req,res,next);
+}catch(error){
+
+}
+}
+*/
