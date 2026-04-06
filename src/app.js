9d9d47a3d803e348988f20e1183e2b9e3b877920
Kalpesh wagh <186034915+kalpesh-73@users.noreply.github.com>
2026-04-06 18:12:01 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,4 +10,9 @@ app.use(express.json({limit:"20kb"}));
 app.use(express.urlencoded({extended:true,limit:"20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser());
+
+
+//routes import
+import userRouter from './routes/user.Router.js'
+app.use("/api/v1/users",userRouter)
 export default app;
