43b0f6e70b67d3b914ffbe9670bf524141708190
Kalpesh wagh <186034915+kalpesh-73@users.noreply.github.com>
2026-05-04 13:49:29 +0530

    add refresh token functionality and fix typos in user authentication

--- src/routes/user.Router.js
+++ src/routes/user.Router.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser ,refreshAccessToken} from "../controllers/user.controllers.js";
 import {upload} from "../middleware/Multer.js"
 import  {verifyJWT} from "../middleware/auth.middleware.js"
 const router=Router()
@@ -18,5 +18,5 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
-
+router.route("/refresh-token").post(refreshAccessToken)
 export default router
